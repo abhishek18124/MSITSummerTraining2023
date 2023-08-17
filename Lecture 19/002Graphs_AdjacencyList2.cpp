1bfce70623555fef3e7216d6c64ac0041400c8d8
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-08-17 13:39:03 +0530

    Lecture 19

---
+++ Lecture 19/002Graphs_AdjacencyList2.cpp
@@ -0,0 +1,69 @@
+/*
+
+Implementation of the graph using adjacency list.
+
+Note : each vertex in the graph is generic.
+
+*/
+
+#include<iostream>
+#include<unordered_map>
+#include<list>
+
+using namespace std;
+
+template <typename T>
+class graph {
+
+	unordered_map<T, list<T>> neighbourMap; // to store the graph representation
+	bool isDirected; // to indicate if the graph is directed or not
+
+public :
+
+	graph(bool isDirected = false) {
+		this->isDirected = isDirected;
+	}
+
+	void addEdge(T u, T v) {
+
+		// adds an edge b/w vertex u and v
+
+		neighbourMap[u].push_back(v);
+		if (!isDirected) {
+			neighbourMap[v].push_back(u);
+		}
+
+	}
+
+	void print() {
+
+		// prints the adjacency list representation of the graph
+
+		for (pair<T, list<T>> vertex : neighbourMap) {
+			T vertexLabel = vertex.first;
+			list<T> neighbourList = vertex.second;
+			cout << vertexLabel << " : ";
+			for (T neighbour : neighbourList) {
+				cout << neighbour << " ";
+			}
+			cout << endl;
+		}
+
+	}
+};
+
+int main() {
+
+	graph<int> g;
+
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 3);
+	g.addEdge(2, 3);
+	g.addEdge(2, 4);
+	g.addEdge(3, 4);
+
+	g.print();
+
+	return 0;
+}
